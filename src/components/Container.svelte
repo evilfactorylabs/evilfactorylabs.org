6f5964cce6ebf3cd6ac692e50a5f865c75758db8
Fariz Rizaldy <fariz@evilfactory.id>
2020-01-14 03:24:19 +0700

    feat: create container component

---
+++ src/components/Container.svelte
@@ -0,0 +1,26 @@
+<style>
+  .c-container {
+    width: 100%;
+    height: 100vh;
+    display: table;
+    margin-top: 4rem;
+  }
+
+  .c-content {
+    padding: 1rem 2rem;
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  @media screen and (min-width: 60em) {
+    .c-content {
+      padding: 1.8rem 5rem;
+    }
+  }
+</style>
+
+<div class="c-container">
+  <div class="c-content">
+    <slot />
+  </div>
+</div>
