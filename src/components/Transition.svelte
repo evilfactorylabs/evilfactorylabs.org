4afaf5be81d0dab80bad5cc3d1265abf8da2f34d
Fariz Rizaldy <fariz@evilfactory.id>
2020-01-14 03:25:28 +0700

    feat: create Transition component

---
+++ src/components/Transition.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from 'svelte'
+  import { fade } from 'svelte/transition'
+
+  let isMounted = false
+
+  onMount(() => {
+    isMounted = true
+  })
+</script>
+
+{#if isMounted}
+  <div transition:fade={{ duration: 500 }}>
+    <slot />
+  </div>
+{/if}
